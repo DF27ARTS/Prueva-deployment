18f0b0110aafd97a478534a420ff95d384af5ba7
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-08-17 18:34:12 -0500

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
+const app = require('./src/app.js');
 
 const port = process.env.PORT || 3001;
-
-app.get('/hello', (req, res) =>{
-  res.json({saludo: 'Hello everyone'});
-})
 
 app.listen(port, ()=> {
   console.log(`listening on port${port}`)
