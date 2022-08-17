16d56d63885c93810215c4650ff8feb7cdaae4df
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-08-17 18:56:10 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) =>{
+app.get('/', (req, res) =>{
   res.json({saludo: 'Hello everyone'});
+})
+
+app.get('/hello', (req, res) =>{
+  res.json({saludo: 'Hello everyone one again'});
 })
 
 module.exports = app;
