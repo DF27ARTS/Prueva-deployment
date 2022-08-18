bc004f54c43b3c7572cb3898bc26549d1265e8a3
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-08-17 19:05:53 -0500

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const app = require('./src/app.js');
+// const app = require('./src/app.js');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+  res.json({saludo: 'Hello everyone once more'});
+})
 
 const port = process.env.PORT || 3001;
 
@@ -6,3 +12,14 @@ app.listen(port, ()=> {
   console.log(`listening on port${port}`)
 })
 
+// {
+//   "routes": [
+//     { "src": "/redirect", "status": 308, "headers": { "Location": "https://example.com/" } },
+//     { "src": "/custom-page", "headers": {"cache-control": "s-maxage=1000"}, "dest": "/index.html" },
+//     { "src": "/api", "dest": "/my-api.js" },
+//     { "src": "/users", "methods": ["POST"], "dest": "/users-api.js" },
+//     { "src": "/users/(?<id>[^/]*)", "dest": "/users-api.js?id=$id" },
+//     { "src": "/legacy", "status": 404},
+//     { "src": "/.*", "dest": "https://my-old-site.com"}
+//   ]
+// }
